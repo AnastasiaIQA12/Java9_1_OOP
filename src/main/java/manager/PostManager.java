600c8da694aca84c056547dd4c8bfc32c8236b2c
AnastasiaIQA12 <Sinka8900@mail.ru>
2020-11-27 12:42:49 +0300

    v3

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] searchPost(String query){
+        //логика поиска поста по поисковому запросу
+        return null;
+    }
+
+    public void deletePost(int id){
+        //логика удаления поста по id
+
+    }
+}
