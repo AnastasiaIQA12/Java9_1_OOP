7db2e8430a37f8aa02777cd7a4a2b85f4844f79d
AnastasiaIQA12 <Sinka8900@mail.ru>
2020-11-27 11:49:35 +0300

    v2

--- src/main/java/domain/LikesInfo.java
+++ src/main/java/domain/LikesInfo.java
@@ -2,5 +2,6 @@ package domain;
 
 public class LikesInfo {
     private int count;//количество лайков
+    private int likesUser;//наличие отметки Мне нравиться от пользователя
     private boolean canLike;//может ли пользователь поставить лайк
 }
--- src/main/java/domain/ReportsInfo.java
+++ src/main/java/domain/ReportsInfo.java
@@ -3,5 +3,5 @@ package domain;
 public class ReportsInfo {
     private int count;//количество репостов
     private boolean canReport;//может ли пользователь оставить репост
-    private int[] typeRepot;//тип репоста:0-Отправить другу, 1-Опубликовать в группе, 2- отправить ссылку в
+    private int repostedUser;//наличие репоста от теущего пользователя
 }
